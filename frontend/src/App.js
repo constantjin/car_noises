250ee01db723cd78f5eec12db906bb2d746da042
Sangsoo Jin <constantlyjin@gmail.com>
2021-02-07 16:29:37 +0900

    Add an automatic screen height adjustment script

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Register from "./pages/Register";
@@ -7,8 +8,22 @@ import Rating from "./pages/Rating";
 import End from "./pages/End";
 
 function App() {
+  const resize = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    console.log("resized");
+  };
+
+  useEffect(() => {
+    resize();
+    window.addEventListener("resize", () => resize());
+  }, []);
+
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-center">
+    <div
+      className="flex flex-col h-screen justify-center items-center text-center"
+      style={{ height: "calc(var(--vh, 1vh) * 100)" }}
+    >
       <Router>
         <Switch>
           <Route exact={true} path="/" component={Register} />
